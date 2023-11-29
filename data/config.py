775f93f542b501dc4c26429cc028d178ea465f6c
san1rope <valetinles@gmail.com>
2023-11-29 23:11:44 +0200

    Update 2

---
+++ data/config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = '6871455188:AAFMt3zkM_P65WycsL-MV3Iky-QDrFvFsgI'
+
+ADMINS = [1643710214]
+#Адмін 6128561399, крмпанія в смартфоні 5791823682
+
+#DATABASE_URL = 'postgres://uhiiehvmiqvcav:0bb9bd695847d63b4e04dc722a4d103dccc1ed46fb9782e730548bda02b3218a@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d9u2koal6bthk5'
+# в utils/db/db_loader.py
+
+sale_photo = 'https://i.ibb.co/x8LsRsk/1000-1000-29.jpg'
+adress = ''
